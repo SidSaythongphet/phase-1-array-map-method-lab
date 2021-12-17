84fd0cb7d52257f688004e66c6866665ecda4255
Sid Saythongphet <sid.saythongphet@gmail.com>
2021-12-16 21:14:55 -0500

    commit tree 0d98fe379c563f875b2ed1e36a099161c1cd719b

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function letterCap(titles) { 
+  let words = titles.split(' ')
+  return words.map(word => (word[0].toUpperCase() + word.slice(1, word.length))).join(" ")
 }
+
+function titleCased() {
+  return tutorials.map(letterCap)
+}
+
+console.log(tutorials)
+console.log(titleCased(tutorials))
